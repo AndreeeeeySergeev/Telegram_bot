85f2c323f57893a982c658aa38dbe30a1fbb562e
Andrey Sergeev <148659604+Andreyergeev@users.noreply.github.com>
2024-01-18 12:56:50 +0900

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = '6788286449:AAGZOjrzdT0saWFbu0Pe9BnpJvSEaCs7nOs'
 
-keys = {'Евро': 'EUR',
-        'Рубль': 'RUB',
-        'Доллар': 'USD',
+keys = {'евро': 'EUR',
+        'рубль': 'RUB',
+        'доллар': 'USD',
         }
